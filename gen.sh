821b1b4a93320697b0cb7d464befaca345fc49b9
Confiks <confiks@scriptbase.org>
2021-03-09 16:38:07 +0100

    feat: Added differentiation between ECC modes L and Q

--- gen.sh
+++ gen.sh
@@ -11,10 +11,13 @@ do
 	cat $i.bin |  base64 > $i.base64
 
 	# comparison: Q is 25% error correction - closest to aztec 23%
-	qrencode -l Q -m 0 -s 1 -8 -o  $i.qr8.png -r $i.bin
-	qrencode -l Q -m 0 -s 1 -i -o  $i.qr2.png -r $i.base45
+	qrencode -l L -m 0 -s 1 -8 -o  $i.qrL8.png -r $i.bin
+	qrencode -l L -m 0 -s 1 -i -o  $i.qrL2.png -r $i.base45
+	qrencode -l Q -m 0 -s 1 -8 -o  $i.qrQ8.png -r $i.bin
+	qrencode -l Q -m 0 -s 1 -i -o  $i.qrQ2.png -r $i.base45
 
 	# 23% error correction is the default.
-	cat $i.bin | ~/bin/aztec-png -b 0 -s 1 -f - $i.az.png
+	cat $i.bin | ~/bin/aztec-png -c 7 -b 0 -s 1 -f - $i.azL.png
+	cat $i.bin | ~/bin/aztec-png -b 0 -s 1 -f - $i.azQ.png
 done
 
